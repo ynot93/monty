fb40246f92321579548e6057e2e31fb74a954951
millyanne93 <nmillyanne20@gmail.com>
2023-10-18 09:57:26 -0700

    Add sub opcode

--- monty.h
+++ monty.h
@@ -49,5 +49,6 @@ void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
 
 #endif
