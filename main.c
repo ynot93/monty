47b86e335f819be1620e1a83b27c48f29abe265b
millyanne93 <nmillyanne20@gmail.com>
2023-10-22 23:31:53 -0700

    Correct error message

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open the file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((read = getline(&line, &len, file)) != -1)
